551df224f61f08c5a1120b261fb0e597263d7a5f
Lucas Domene Firmo <lucas.firmo@gigigo.com.br>
2018-05-05 14:53:07 -0300

    Adding initial Xcode project

---
+++ MarvelHeroes/MarvelHeroes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Lucas Domene on 05/05/18.
+//  Copyright © 2018 Lucas Domene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

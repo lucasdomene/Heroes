12cf1a6960f0ee16cbf56040d3dc239ba7658f9c
Lucas Domene Firmo <lucas.firmo@gigigo.com.br>
2018-05-05 15:08:05 -0300

    Adding Hero and Thumbnail models

---
+++ MarvelHeroes/MarvelHeroes/Models/Hero.swift
@@ -0,0 +1,18 @@
+//
+//  Hero.swift
+//  MarvelHeroes
+//
+//  Created by Lucas Domene on 05/05/18.
+//  Copyright © 2018 Lucas Domene. All rights reserved.
+//
+
+import Foundation
+
+struct Hero {
+	
+	let id: Int
+	let name: String
+	let description: String
+	let thumbnail: Thumbnail
+	
+}
---
+++ MarvelHeroes/MarvelHeroes/Models/Thumbnail.swift
@@ -0,0 +1,16 @@
+//
+//  Thumbnail.swift
+//  MarvelHeroes
+//
+//  Created by Lucas Domene on 05/05/18.
+//  Copyright © 2018 Lucas Domene. All rights reserved.
+//
+
+import Foundation
+
+struct Thumbnail {
+	
+	let path: String
+	let imageExtension: String
+	
+}

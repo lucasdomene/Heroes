83fa676982588bdf0082f4f25d96a3777594ece4
Lucas Domene Firmo <lucas.firmo@gigigo.com.br>
2018-05-07 17:26:09 -0300

    Fetching all data on details view

---
+++ MarvelHeroes/MarvelHeroes/View Controllers/Details/DetailsViewController+DataSource.swift
@@ -0,0 +1,26 @@
+//
+//  DetailsViewController+DataSource.swift
+//  MarvelHeroes
+//
+//  Created by Lucas Domene on 07/05/18.
+//  Copyright © 2018 Lucas Domene. All rights reserved.
+//
+
+import UIKit
+
+extension DetailsViewController: UITableViewDataSource {
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return details.count
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "detailsCell", for: indexPath)
+		return cell
+	}
+	
+}
--- MarvelHeroes/MarvelHeroes/View Controllers/Details/DetailsViewController.swift
+++ MarvelHeroes/MarvelHeroes/View Controllers/Details/DetailsViewController.swift
@@ -14,19 +14,72 @@ class DetailsViewController: UIViewController {
 	@IBOutlet weak var tableView: UITableView!
 	
 	var hero: Hero!
+	var details = [Int: [Any]]()
+	
+	enum Sections: Int {
+		case comics = 0
+		case stories
+		case events
+		case series
+	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tableView.tableFooterView = UIView()
-		
-		DetailsService.fetchDetails(type: Serie.self, offset: 0, heroID: String(hero.id)) { result in
+		initDetails()
+		fetchComics()
+		fetchEvents()
+		fetchSeries()
+		fetchStories()
+	}
+	
+	func initDetails() {
+		for i in 0...3 {
+			details[i] = []
+		}
+	}
+	
+	func fetchComics() {
+		DetailsService.fetchDetails(type: Comic.self, offset: 0, heroID: String(hero.id)) { result in
 			switch result {
 			case .success(let comics):
-				print(comics)
+				self.details[Sections.comics.rawValue] = comics
 			case .failure(let error):
 				print(error)
 			}
 		}
-		
+	}
+	
+	func fetchEvents() {
+		DetailsService.fetchDetails(type: Event.self, offset: 0, heroID: String(hero.id)) { result in
+			switch result {
+			case .success(let events):
+				self.details[Sections.events.rawValue] = events
+			case .failure(let error):
+				print(error)
+			}
+		}
+	}
+	
+	func fetchSeries() {
+		DetailsService.fetchDetails(type: Serie.self, offset: 0, heroID: String(hero.id)) { result in
+			switch result {
+			case .success(let series):
+				self.details[Sections.series.rawValue] = series
+			case .failure(let error):
+				print(error)
+			}
+		}
+	}
+	
+	func fetchStories() {
+		DetailsService.fetchDetails(type: Story.self, offset: 0, heroID: String(hero.id)) { result in
+			switch result {
+			case .success(let stories):
+				self.details[Sections.stories.rawValue] = stories
+			case .failure(let error):
+				print(error)
+			}
+		}
 	}
 }
